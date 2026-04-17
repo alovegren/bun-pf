7d6493483d59594d0488dc5100fd00cd979a1cda
Missy Lovegren <audrey.lovegren@gmail.com>
2026-04-17 08:49:23 -0500

    Add Google Form. Update title to Missy

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import styles from '../styles/About.module.css'
 import RuntimeEmail from './RuntimeEmail'
+import EmailForm from './EmailForm'
 
 const About: React.FC = () => {
   return (
@@ -11,12 +12,9 @@ const About: React.FC = () => {
         <p className={styles.description}>
           I'm Missy, an artist based in Austin, Texas. Please feel free to get in touch with me at <RuntimeEmail />.
         </p>
-
-        <form className={styles.signupForm}>
+        <EmailForm />
+        {/* <form className={styles.signupForm}>
           <label htmlFor="email" className={styles.formLabel}>Mailing list</label>
-          <p className={styles.formDescription}>
-            If you want to be updated when I do an artist thing, drop your email below. I'll only email you if I start something new, like an online shop or a mail club. I'm facing email bankruptcy myself, so you can be assured I will not spam or market to you.
-          </p>
           <div className={styles.formRow}>
             <input
               id="email"
@@ -28,7 +26,7 @@ const About: React.FC = () => {
             />
             <button type="submit" className={styles.submitButton}>Submit</button>
           </div>
-        </form>
+        </form> */}
       </section>
     </main>
   )
